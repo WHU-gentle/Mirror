4eedcb67188338796da8051036f9da0708f613af
cheng酱 <38248283+San3132308912@users.noreply.github.com>
2019-06-25 08:50:40 +0800

    Update scan.js

--- Face/static/js/scan.js
+++ Face/static/js/scan.js
@@ -92,17 +92,106 @@ ring.setOption({
             itemStyle : labelFromatter,
             data : [
                 {name:'other', value:100-data3, itemStyle : labelBottom},
-                {name:'颜值', value:data3,itemStyle : labelTop}
+                {name:'细腻度', value:data3,itemStyle : labelTop}
             ]
         }
     ]
 }) ;
 
+var ring = echarts.init(document.getElementById('ring1'));
+var data4=74;
+var data5=88;
+var data6=85;
+var value=[];
+value.push(data4);
+value.push(data5);
+value.push(data6);
+var labelTop = {
+    normal : {
+        label : {
+            show : true,
+            position : 'center',
+            formatter : '{b}',
+            textStyle: {
+                baseline : 'bottom',
+			    fontsize: '20'
+            }
+        },
+        labelLine : {
+            show : false
+        }
+    }
+};
+
+var labelFromatter = {
+    normal : {
+        label : {
+            formatter : function (params){
+                return 100 - params.value
+            },
+            textStyle: {
+                baseline : 'top',
+				fontSize:'20'
+				
+            }
+        }
+    },
+}
+var labelBottom = {
+    normal : {
+        color: '#ccc',
+        label : {
+            show : true,
+            position : 'center'
+        },
+        labelLine : {
+            show : false
+        }
+    },
+    emphasis: {
+        color: 'rgba(0,0,0,0)'
+    }
+};
+var radius = [50, 65];
+ring.setOption({
+    color:["#ffb1bf","#a0abff","#00b384"],
+    series : [
+        {
+            type : 'pie',
+            center : ['15%', '58%'],
+            radius : radius,
+            x: '0%', // for funnel
+            itemStyle : labelFromatter,
+            data : [
+                {name:'other', value:100-data4, itemStyle : labelBottom},
+                {name:'年轻度', value:data1,itemStyle : labelTop}
+            ]
+        },
+        {
+            type : 'pie',
+            center : ['45%', '58%'],
+            radius : radius,
+            x:'20%', // for funnel
+            itemStyle : labelFromatter,
+            data : [
+                {name:'other', value:100-data5, itemStyle : labelBottom},
+                {name:'健康度', value:data2,itemStyle : labelTop}
+            ]
+        },
+        {
+            type : 'pie',
+            center : ['75%', '58%'],
+            radius : radius,
+            x:'40%', // for funnel
+            itemStyle : labelFromatter,
+            data : [
+                {name:'other', value:100-data6, itemStyle : labelBottom},
+                {name:'油干性', value:data3,itemStyle : labelTop}
+            ]
+        }
+    ]
+}) ;
 window.onload=function(){
  "use strict";
   document.getElementsByid("wrinkle")[0].value=98;
 };
-
-
-
-
